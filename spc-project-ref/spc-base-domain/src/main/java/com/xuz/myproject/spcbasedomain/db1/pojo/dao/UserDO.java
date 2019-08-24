0236e8f52f0cd579550d38ffb5a7ecef7963d402
xuz10086 <xuz10086@icloud.com>
2019-08-24 09:32:49 +0800

    pom文件修改：1.打包方式；2.maven-plugin插件；3.依赖调整

--- spc-project-ref/spc-base-domain/src/main/java/com/xuz/myproject/spcbasedomain/db1/pojo/dao/UserDO.java
+++ spc-project-ref/spc-base-domain/src/main/java/com/xuz/myproject/spcbasedomain/db1/pojo/dao/UserDO.java
@@ -1,10 +1,7 @@
 package com.xuz.myproject.spcbasedomain.db1.pojo.dao;
 
-import lombok.Data;
-
 import java.io.Serializable;
 
-@Data
 public class UserDO implements Serializable {
     private static final long serialVersionUID = 7854837466417999254L;
 
@@ -17,4 +14,44 @@ public class UserDO implements Serializable {
     private Integer openId;
 
     private String email;
+
+    public Integer getlUserId() {
+        return lUserId;
+    }
+
+    public void setlUserId(Integer lUserId) {
+        this.lUserId = lUserId;
+    }
+
+    public String getVcUserName() {
+        return vcUserName;
+    }
+
+    public void setVcUserName(String vcUserName) {
+        this.vcUserName = vcUserName;
+    }
+
+    public String getVcUserPassword() {
+        return vcUserPassword;
+    }
+
+    public void setVcUserPassword(String vcUserPassword) {
+        this.vcUserPassword = vcUserPassword;
+    }
+
+    public Integer getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(Integer openId) {
+        this.openId = openId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
